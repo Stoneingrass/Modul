c7bd1f18d2191af4eba4da0dffeca6a8e26f6a0e
Illya <svirillya@gmail.com>
2022-10-20 13:57:07 +0300

    x

--- src/modul1/Main.java
+++ src/modul1/Main.java
@@ -1,3 +1,5 @@
+package modul1;
+
 public class Main {
     public static void main(String[] args) {
         int[][] array = new int[5][8];
@@ -17,21 +19,19 @@ public class Main {
         //array output
         System.out.println("The array:");
 
-        for (int i=0; i< array.length; i++) {
-            for (int j=0; j< array[i].length; j++)
-            {
-                System.out.print(array[i][j] + "\t");
+        for (int[] ints : array) {
+            for (int anInt : ints) {
+                System.out.print(anInt + "\t");
             }
             System.out.println();
         }
 
         //max element search
         int maxElement=array[0][0];
-        for (int i=0; i< array.length; i++) {
-            for (int j=0; j< array[i].length; j++)
-            {
-                if (maxElement<array[i][j]) {
-                    maxElement=array[i][j];
+        for (int[] ints : array) {
+            for (int anInt : ints) {
+                if (maxElement < anInt) {
+                    maxElement = anInt;
                 }
             }
         }
